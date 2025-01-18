936c6c6e23a2ebd3cfca3aafcbf934dd5887b495
rowan oliver weaver <159619990+RooOliver-Weaver@users.noreply.github.com>
2025-01-18 23:50:59 +0000

    models generated and migrated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+Book.destroy_all
+Author.destroy_all
+
+10.times do
+  author = Author.create!(
+    name: Faker::Book.author
+  )
+
+  rand(2..5).times do
+    Book.create!(
+      title: Faker::Book.title,
+      description: Faker::Lorem.paragraph(sentence_count: 7),
+      cover: "https://via.placeholder.com/150",
+      year: rand(1900..2023),
+      rating: rand(3.0..5.0).round(1),
+      isbn: Faker::Number.unique.number(digits: 13).to_s,
+      read: [ true, false ].sample,
+      author: author
+    )
+  end
+end
+
+puts "Seeded #{Author.count} authors and #{Book.count} books!"
